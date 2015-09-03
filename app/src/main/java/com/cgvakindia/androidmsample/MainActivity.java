65de16e3f39324e62b9c175b54b7606c1e2e5c96
Prakash <prakashgtup@gmail.com>
2015-09-03 15:12:10 +0530

    System out added for checking

--- app/src/main/java/com/cgvakindia/androidmsample/MainActivity.java
+++ app/src/main/java/com/cgvakindia/androidmsample/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("test");
+        System.out.print("test"); 
     }
 
     @Override
